4b18f99c83333a00342d965e7248137a6bdc491f
Tewari <devtewari@deloitte.com>
2022-02-18 15:03:35 +0530

    Added cost node js api

--- modules/Cost.js
+++ modules/Cost.js
@@ -2,8 +2,11 @@ const cost=require('../Data/Data.js').cost;
 const express = require('express');
 const route = express.Router();
 
+console.log(cost); //to calculate the cost model
 route.get('/api/cost',(req,res)=>{
+
 res.send(cost);
 })
+
 
 module.exports = route;
